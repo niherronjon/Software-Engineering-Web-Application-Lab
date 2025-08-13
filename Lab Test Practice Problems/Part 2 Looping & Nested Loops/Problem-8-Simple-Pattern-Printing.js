bbbae81e4cb53014a5e67f740c143b778678c401
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-08-13 21:28:37 +0600

    Update Problem-8-Simple-Pattern-Printing.js

--- Lab Test Practice Problems/Part 2 Looping & Nested Loops/Problem-8-Simple-Pattern-Printing.js
+++ Lab Test Practice Problems/Part 2 Looping & Nested Loops/Problem-8-Simple-Pattern-Printing.js
@@ -1,8 +1,10 @@
 // Print 5x5 square of asterisks
 
-for (let row = 1; row <= 5; row++) {
+for (let row = 1; row <= 5; row++)
+{
     let line = "";
-    for (let col = 1; col <= 5; col++) {
+    for (let col = 1; col <= 5; col++)
+    {
         line += "*";
     }
     console.log(line);
